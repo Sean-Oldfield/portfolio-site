06e3d0ad7f6fc69067f2f89ac12bd220c3c8d608
Sean Oldfield <seanoldfield95@gmail.com>
2021-07-23 10:16:14 +0100

    added hover effects and transitions

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = new Vue({
                 company: 'Surple Energy',
                 title: 'Founder & Full-Stack Software Developer',
                 dates: 'Jan 2018 - Present',
+                url: 'https://surple.co.uk',
                 desc: [
                     "Developing and maintaining the company's energy analytics product which helped businesses to understand their energy use.",
                     "Working with a small team of developers to bring this new product to life, entirely from scratch.",
@@ -19,6 +20,7 @@ var app = new Vue({
                 company: 'The Alacrity Foundation',
                 title: 'Technical Lead',
                 dates: 'Aug 2017 - Oct 2018',
+                url: 'https://alacrityfoundation.co.uk/',
                 desc: [
                     "The Alacrity Foundation is an entrepreneurship course designed to empower graduates to create the next generation of technology companies.",
                     "During my time at Alacrity, I received mentorship from more than 70 world-class mentors in various fields, such as entrepreneurship, software development, marketing, and business administration.",
@@ -30,6 +32,7 @@ var app = new Vue({
                 company: 'Software Development Bootcamp',
                 title: 'Technical Lead',
                 dates: 'Jun 2017 - Aug 2017',
+                url: 'https://alacrityfoundation.co.uk/',
                 desc: [
                     "Before joining The Alacrity Foundation, everything I knew about software development was self-taught. To prove myself, I had to complete an eight-week software development bootcamp.",
                     "I successfully completed the bootcamp after building and presenting my final project — a web app that helped with recruitment. This secured my place on The Alacrity Foundation's entrepreneurship programme.",
@@ -40,6 +43,7 @@ var app = new Vue({
                 company: 'Swansea University',
                 title: 'Biology Undergraduate',
                 dates: 'Sep 2014 - Jun 2017',
+                url: 'https://www.swansea.ac.uk/',
                 desc: [
                     "Whilst studying Biology at Swansea University, I achieved First Class Honours and excelled at technology-based modules.",
                     "My strongest grades came from the Advanced Data Analysis (84%) and Data Analysis & Presentation (84%) modules. This involved using the statistical programming language R to analyse and visualise biological data."
